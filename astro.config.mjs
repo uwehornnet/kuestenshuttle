da5ee599d1bfd2e2884d11e6059cd1c68f34cf9e
uhorn <uwehornnet@users.noreply.github.com>
2025-01-15 17:07:06 +0100

    added vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+  integrations: [react()],
+  adapter: vercel(),
 });
